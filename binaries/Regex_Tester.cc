4b249922ae1e20d63ab03e49a16bd2e5fa098305
Hal Clark <gmail.com@hdeanclark>
2019-09-06 16:41:55 -0700

    Regex_Tester: support passing source at invocation time.

--- binaries/Regex_Tester.cc
+++ binaries/Regex_Tester.cc
@@ -4,19 +4,26 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
 
 #include "YgorFilesDirs.h"
 #include "YgorString.h"
 
+
 int main(int argc, char **argv){
     std::string Source;
 
-    //Check to see if a file has been passed, containing the sample text.
+    // Check to see if a file has been passed, containing the sample text.
     if((argc == 2) && Does_File_Exist_And_Can_Be_Read(argv[1])){
         Source = LoadFileToString(argv[1]);
+
+    // Check to see if the sample test has already been provided.
+    }else if(argc == 2){
+        Source = argv[1];
+        
     }else{
         std::cout << "Input the source text (or representative text to be regexed.)" << std::endl;
-        std::cout << "NOTE: Source text can also be passed in as a filename argument." << std::endl;
+        std::cout << "NOTE: Source text can also be passed in as a filename argument or as an argument to this program." << std::endl;
         std::getline(std::cin, Source);
     }
 
@@ -25,15 +32,19 @@ int main(int argc, char **argv){
     while(true){
         std::cout << "Now input the regex. The matches will be shown." << std::endl;
 
-        std::string Regex;
-        std::getline(std::cin, Regex);
+        std::string regex_str;
+        std::getline(std::cin, regex_str);
 
-        const auto matches = GetAllRegex2(Source, Regex);
+        const auto subs = GetAllRegex2(Source, regex_str);
 
-        std::cout << "Found " << matches.size() << " matches." << std::endl;
+        std::regex compiled_regex( regex_str, std::regex::icase );
+        const auto w_match = std::regex_match(Source, compiled_regex);
 
-        for(const auto & matche : matches){
-            std::cout << "    " << matche << std::endl;
+        std::cout << "Global match? --> " << (w_match ? "true" : "false") << std::endl;
+        std::cout << "Subs found " << subs.size() << " matches." << std::endl;
+
+        for(const auto & sub : subs){
+            std::cout << "    " << sub << std::endl;
         }
     }
 
