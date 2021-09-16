8dd0ebbd2393a9f57f0728cb90d996671a7307bf
Haley Clark <gmail.com@hdeanclark>
2021-09-15 22:48:29 -0700

    YgorArguments: rewrite argument parser.
    This rewrite does away with getopt(), getopt_long(), and the portability madness that it created. Minimal testing was
    performed, but it seems to reproduce the previous behaviour in a few integration tests I ran. Might have to revisit to
    tweak edge cases.

--- src/YgorArguments.h
+++ src/YgorArguments.h
@@ -1,5 +1,4 @@
-//YgorArguments.h - A wrapper around getopt (or similar) which uses a more-DRY-like method of handling main's arguments.
-//                Common arguments, like '--help' are provided automatically.
+//YgorArguments.h - A portable getopt_long()-like parser that uses a more-DRY-like method of handling arguments.
 
 #ifndef YGOR_ARGUMENTS_PROJECT_UTILITIES_HC_
 #define YGOR_ARGUMENTS_PROJECT_UTILITIES_HC_
@@ -14,32 +13,14 @@
 #include <functional>
 #include <set>
 
-#include <unistd.h>            //Needed for getopt().
-#if defined(_GNU_SOURCE)
-    #include <getopt.h>            //Needed for getopt_long() which is a GNU extension.
-#endif //defined(_GNU_SOURCE)
-
 #include "YgorDefinitions.h"
 #include "YgorMisc.h"            //Needed for function macros FUNCINFO, FUNCWARN, FUNCERR.
 #include "YgorString.h"
 #include "YgorEnvironment.h"     //Needed for Get_Terminal_Char_Dimensions();
 
 
-// #########################################################################
-// ##### NOTE:                                                       #######
-// #####  This typedef has been made so that some compiler errors    #######
-// #####  can be mitigated in gcc 4.8.1. If one uses std::make_tuple #######
-// #####  with a 'bare' closure, then a long, nasty error is given   #######
-// #####  about: "/usr/include/c++/4.8.1/functional: In substitution #######
-// #####  of ‘template<class _Res, class ... _ArgType  ........"     #######
-// #####  Using this typedef solves the problem. I'm not sure if it  #######
-// #####  should stay or not, though. It sort of hides the fact that #######
-// #####  it is a simple tuple.                                      #######
-// #####                                                             #######
-// #####  Remove this note and the typedef if it is desired, or      #######
-// #####  just this note otherwise.                                  #######
-// #########################################################################
-typedef std::tuple<long int,char,std::string,bool,std::string,std::string,std::function<void(const std::string &optarg)>> ygor_arg_handlr_t;
+typedef std::function<void(const std::string &optarg)> ygor_arg_functor_t;
+typedef std::tuple<long int,char,std::string,bool,std::string,std::string,ygor_arg_functor_t> ygor_arg_handlr_t;
 
 class ArgumentHandler {
     private:
@@ -57,7 +38,7 @@ class ArgumentHandler {
         //
         // Key:  family, short form, long form, takes argument?, sample argument, description, callback function.
         //         0         1           2             3               4               5               6
-        std::list< std::tuple< long int, char, std::string, bool, std::string, std::string, std::function< void (const std::string &optarg) > > > directives;
+        std::list< std::tuple< long int, char, std::string, bool, std::string, std::string, ygor_arg_functor_t > > directives;
 
         std::function< void (int, const std::string &optarg) > default_callback; //Gets called by default, when no others matches are found. 
         std::function< void (const std::string &optarg) > optionless_callback;   //Gets called for each optionless argument passed into main.
@@ -79,7 +60,7 @@ class ArgumentHandler {
             };
  
             //Register the special --help directive.
-            auto help_callback = [&](const std::string &optarg) -> void {
+            auto help_callback = [&](const std::string &) -> void {
                 //------------------------------------------------------ help output ---------------------------------------------------
                 //Environmental information and common things.
 #ifdef YGOR_USE_LINUX_SYS
@@ -179,14 +160,12 @@ class ArgumentHandler {
 
 
         //Member functions.
-        void push_back( const std::tuple< long int, char, std::string, bool, std::string, std::string, std::function< void (const std::string &optarg) > > &directive){
+        void push_back( const std::tuple< long int, char, std::string, bool, std::string, std::string, ygor_arg_functor_t > &directive){
             //Check for conflicting options??
 
             // ...
  
             directives.push_back( directive );
-
-
             return;
         };
 
@@ -196,78 +175,158 @@ class ArgumentHandler {
             //Check if there is anything to do.
             if(local_argc <= 1) return true;
 
-            //First, we set up the getopt stuff.
-            std::string short_opts;
-            for(auto d_it = directives.begin(); d_it != directives.end(); ++d_it){
-                short_opts += std::get<1>(*d_it);
-                if( std::get<3>(*d_it) == true ) short_opts += ":";
+            // Extract the tokens.
+            std::list<std::string> tokens;
+            for(int i = 1; i < local_argc; ++i){
+                tokens.emplace_back( std::string(local_argv[i]) );
+                tokens.back() = Canonicalize_String2( tokens.back(), CANONICALIZE::TRIM_ENDS );
             }
 
-#if defined(_GNU_SOURCE)
-            std::vector<struct option> long_opts;
-            for(auto d_it = directives.begin(); d_it != directives.end(); ++d_it){
-                //long_opts.push_back( { description, (int)(takes arg?), nullptr, (char)(short form) } );
-                const int takesarg = (std::get<3>(*d_it) == true) ? 1 : 0;
-                const struct option shuttle = { (std::get<2>(*d_it)).c_str(), takesarg, nullptr, std::get<1>(*d_it) };
-                long_opts.push_back( shuttle );
-            }
-            {
-                const struct option shuttle = { NULL, 0, NULL, 0 };
-                long_opts.push_back( shuttle ); //The manual says to terminate with all-zeros.
-            }
-            const std::vector<struct option> long_const_opts( long_opts );
-            long_opts.clear(); //So we do not accidentally invalidate the pointer to the vector's data.
-#endif // defined(_GNU_SOURCE)
+            // Combine tokens.
+            const auto explode = [](const std::string &s){
+                const auto N = s.size();
+                const auto has_dash_1 = (0UL < N) && (s[0] == '-');
+                const auto has_dash_2 = has_dash_1 && (1UL < N) && (s[1] == '-');
 
-            //Now we loop over the arguments, calling the callbacks if necessary. We cycle through the directives 
-            // backward so that more recently-pushed directives get priority over others.
-            int next_options;
-            do{
-#if defined(_GNU_SOURCE)
-                next_options = getopt_long(local_argc, local_argv, short_opts.c_str(), &long_const_opts[0], nullptr);
-#else
-                next_options = getopt(local_argc, local_argv, short_opts.c_str());
-#endif // defined(_GNU_SOURCE)
-                bool matched = false;
-                for(auto d_it = directives.rbegin(); d_it != directives.rend(); ++d_it){
-                    if( next_options == std::get<1>(*d_it) ){
-                        matched = true;
-                        std::string theoptarg;
-                        if(optarg != nullptr) theoptarg = optarg;
-                        if( std::get<6>(*d_it) ){
-                            (std::get<6>(*d_it))( theoptarg );
-                        }else{
-                            FUNCWARN("Encountered argument '" << next_options << "', but there is not a valid callback function to perform. Continuing");
-                        }
-                        break;
+                const auto pos_key = s.find_first_not_of('-');
+
+                // Only search for key-value structure if this is an option (i.e., preceeded by 1-2 dashes).
+                const auto pos_equals = (has_dash_1 || has_dash_2) ? s.find('=') : std::string::npos;
+                const auto has_equals = (pos_equals != std::string::npos);
+
+                std::string key = (pos_key != std::string::npos) ? s.substr(pos_key) : s;
+                std::string val;
+                if( has_dash_1
+                &&  (0UL < N)
+                &&  (pos_key != std::string::npos) ){
+                    key = s.substr(pos_key, pos_equals);
+                    if(has_equals) val = s.substr(pos_equals);
+                }
+                return std::make_tuple(has_dash_1, has_dash_2, key, val);
+            };
+
+            long int processed_tokens = 0;
+            const auto end = std::end(tokens);
+            for(auto s_it = std::begin(tokens); s_it != end; ){
+                // Valid cases to handle:
+                //
+                //   Short-form options:
+                //    1. -a          (a does not accept/require an argument.)
+                //    2. -a=abc      (a accepts/requires an argument.)
+                //    3. -a abc      (a accepts/requires an argument.)
+                //    4. -abc        (none of a, b, or c accept/require arguments.)
+                //    5. -abc=123    (only c accepts/requires arguments.)
+                //    6. -abc 123    (only c accepts/requires arguments.)
+                //
+                //   Long-form options:
+                //    7. --a         (a is a long-form option.)
+                //    8. --abc       (abc is a long-form option.)
+                //    9. --abc=123
+                //   10. --abc 123
+                //
+                //   Other options: (currently these are all treated as default options.)
+                //   11. -
+                //   12. --
+                //   13. abc
+                //   14. abc=123
+                //
+                auto [has_dash_1, has_dash_2, key, val] = explode(*s_it);
+
+                const auto read_ahead_for_argument = [&](){
+                    ++s_it;
+                    if(s_it == end){
+                        throw std::invalid_argument("Option '"_s + key + "' requires an argument, but none were provided");
                     }
-                }
+                    const auto [arg_has_dash_1, arg_has_dash_2, arg_key, arg_val] = explode(*s_it);
+                    if( arg_key.empty()
+                    ||  !arg_val.empty() ){
+                        throw std::invalid_argument("Option '"_s + key + "' requires an argument, but '" + *s_it + "' was provided");
+                    }
+                    return arg_key;
+                };
 
-                if((matched == false) && (next_options != -1)){
-                    std::string theoptarg;
-                    if(optarg != nullptr) theoptarg = optarg;
+                const auto short_form = (key.size() == 1UL) && has_dash_1 && !has_dash_2;
+                const auto long_form  = (1UL < key.size())  && has_dash_1 &&  has_dash_2;
+                if( false ){
+                }else if( short_form || long_form ){ // Cases 1, 2, 3,  7, 8, 9, and 10.
+                    bool found = false;
+                    // Note: cycle through directives backward so that more recently-pushed directives get priority over others.
+                    for(auto d_it = directives.rbegin(); d_it != directives.rend(); ++d_it){
+                        if(
+                            (short_form && (std::get<char>(*d_it) == key.front())) // Cases 1, 2, and 3.
+                        ||
+                            (long_form && (std::get<2>(*d_it) == key)) // Cases 7, 8, 9, and 10.
+                        ){
+                            const auto wants_val = std::get<bool>(*d_it);
+                            const auto has_val = !val.empty();
+                            if(false){
+                            }else if( !wants_val && !has_val ){
+                                // Case 1. Do nothing.
+                            }else if( wants_val && has_val ){
+                                // Case 2. Do nothing.
+                            }else if( wants_val && !has_val ){
+                                // Case 3. Look to next token for val.
+                                val = read_ahead_for_argument();
+                            }else{ // !wants_val && has_val
+                                throw std::invalid_argument("Option '-"_s + key + "' does not accept arguments");
+                            }
+                            // Invoke the user functor.
+                            if( std::get<ygor_arg_functor_t>(*d_it) ){
+                                (std::get<ygor_arg_functor_t>(*d_it))( val );
+                            }else{
+                                throw std::runtime_error("Option '-"_s + key + "' does not have a valid callback handler");
+                            }
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(found){
+                        ++s_it;
+                        ++processed_tokens;
+                        continue;
+                    }
                     if(default_callback){
-                        default_callback( next_options, theoptarg );
-                    }else{
-                        FUNCWARN("Unable to match argument '" << next_options << "', and unable to execute default callback. Continuing");
-                    } 
-                }
+                        default_callback( processed_tokens, key );
+                        ++s_it;
+                        ++processed_tokens;
+                        continue;
+                    }
+                    throw std::runtime_error("No valid callback available to handle unknown option '"_s + *s_it + "'");
 
-            }while(next_options != -1);
-            //From the optind man page:
-            //  "If the resulting value of optind is greater than argc, this indicates a missing option-argument,
-            //         and getopt() shall return an error indication."
-            for( ; optind < local_argc; ++optind){
-                std::string theoptarg;
-                if(local_argv[optind] != nullptr) theoptarg = local_argv[optind];
-                if(optionless_callback){
-                    optionless_callback( theoptarg );
+                }else if( (1UL < key.size()) && has_dash_1 && !has_dash_2 ){ // Cases 4, 5, and 6.
+                    // Convert cases 4, 5, and 6 to multiple case 1s and possibly a case 2 or 3 by splitting the tokens.
+                    decltype(tokens) l_tokens;
+                    for(const auto &c : key) l_tokens.emplace_back("-"_s + c);
+                    tokens.splice(std::next(s_it), l_tokens);
+                    ++s_it;
+                    continue;
+
+                }else if( (key.empty() && has_dash_1 && !has_dash_2) // Case 11.
+                      ||  (key.empty() && has_dash_1 &&  has_dash_2) // Case 12.
+                ){
+                    if(optionless_callback){
+                        optionless_callback( *s_it );
+                        ++s_it;
+                        ++processed_tokens;
+                        continue;
+                    }
+                    throw std::runtime_error("No valid callback available for '"_s + *s_it + "'");
+
+                }else if(!key.empty() && !has_dash_1){ // Cases 13 and 14.
+                    if(optionless_callback){
+                        optionless_callback( key );
+                        ++s_it;
+                        ++processed_tokens;
+                        continue;
+                    }
+                    throw std::runtime_error("No valid callback available for '"_s + *s_it + "'");
+
                 }else{
-                    FUNCWARN("Unable to perform optionless argument processing because callback is not valid. Continuing");
+                    throw std::logic_error("Unexpected input: '"_s + *s_it + "'");
                 }
             }
             return true;
         };
-
 };
+
 #endif 
