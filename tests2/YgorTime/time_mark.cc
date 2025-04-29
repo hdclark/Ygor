b812d3943af564810821433baab55343c184453c
Haley Clark <gmail.com@hdeanclark>
2025-04-29 14:46:51 -0700

    YgorTime: partial support ISO 8601 formatted timestamps.
    Hopefully this will appease KRAZAM's GALACTUS microservice until Omega Star can implement ISO time stamps...

--- tests2/YgorTime/time_mark.cc
+++ tests2/YgorTime/time_mark.cc
@@ -22,6 +22,10 @@ TEST_CASE( "time_mark string IO" ){
     REQUIRE(frac_seconds == 0.123456);
     REQUIRE(tm.Dump_as_string() == "20210829-202726");
 
+    // ISO 8601
+    REQUIRE(tm.Read_from_string("2025-08-29T20:27:26.23456Z", &frac_seconds));
+    REQUIRE(frac_seconds == 0.23456);
+
     REQUIRE(!tm.Read_from_string("2021-0-31 20:27:26")); // Non-existent month.
     REQUIRE(!tm.Read_from_string("2021-09-0 20:27:26")); // Non-existent day.
     REQUIRE(!tm.Read_from_string("2021-09-32 20:27:26")); // Non-existent day.
