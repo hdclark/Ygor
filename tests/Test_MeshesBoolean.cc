599704e3d6e241c54467c5797c6f53fc98ad0d37
Hal Clark <gmail.com@hdeanclark>
2026-05-29 13:52:00 -0700

    Amend MeshesBoolean test to try both BooleanMesh versions.

--- tests/Test_MeshesBoolean.cc
+++ tests/Test_MeshesBoolean.cc
@@ -10,6 +10,7 @@
 #include <YgorLog.h>
 #include <YgorMathIOOBJ.h>
 #include <YgorMeshesBoolean.h>
+#include <YgorMeshesBoolean2.h>
 
 
 int main(int argc, char **argv){
@@ -30,12 +31,19 @@ int main(int argc, char **argv){
     }
 
     // Perform a Boolean operation.
-    auto res = BooleanSubtraction(mesh_B, mesh_A, /*max_depth=*/5, /*boundary_scale=*/0.0);
+    auto res_1 = BooleanSubtraction(mesh_B, mesh_A, /*max_depth=*/5, /*boundary_scale=*/0.0);
+
+    auto res_2 = BooleanSubtraction2(mesh_B, mesh_A, /*snap_eps=*/0.0);
 
     // Attempt to write the resulting mesh to file for debugging/inspection purposes.
     {
-        std::fstream f("/tmp/dcma_mesh_C_output.obj", std::ifstream::out);
-        WriteFVSMeshToOBJ(res, f);
+        std::fstream f("/tmp/dcma_mesh_C_output_1.obj", std::ifstream::out);
+        WriteFVSMeshToOBJ(res_1, f);
+        f.close();
+    }
+    {
+        std::fstream f("/tmp/dcma_mesh_C_output_2.obj", std::ifstream::out);
+        WriteFVSMeshToOBJ(res_2, f);
         f.close();
     }
 
--- tests/compile.sh
+++ tests/compile.sh
@@ -18,13 +18,9 @@ cd "${REPOROOT}/tests/"
 
 
 g++ -std=c++17 Report_Machine_Parameters.cc -o report_machine_parameters -lygor -pthread &
-g++ -std=c++17 Test_Algorithms_01.cc -o test_algorithms_01 -lygor -pthread &
 g++ -std=c++17 Test_Algorithms_02.cc -o test_algorithms_02 -lygor -pthread &
-g++ -std=c++17 Test_Algorithms_03.cc -o test_algorithms_03 -lygor -pthread &
 wait
 
-g++ -std=c++17 Test_Arguments_01.cc -o test_arguments_01 -lygor -pthread &
-g++ -std=c++17 Test_Base64_01.cc -o test_base64_01 -lygor -pthread &
 g++ -std=c++17 Test_Containers_01.cc -o test_containers_01 -lygor -pthread & 
 g++ -std=c++17 Test_Containers_02.cc -o test_containers_02 -lygor -pthread &
 g++ -std=c++17 Test_Environment_01.cc -o test_environment_01 -lygor -pthread &
@@ -40,18 +36,13 @@ wait
 
 g++ -std=c++17 Test_Images_01.cc -o test_images_01 -lygor -pthread &
 g++ -std=c++17 Test_Images_02.cc -o test_images_02 -lygor -pthread -lboost_serialization -lboost_iostreams &
-g++ -std=c++17 Test_Images_03.cc -o test_images_03 -lygor -pthread &
-g++ -std=c++17 Test_Images_Dithering.cc -o test_images_dithering -lygor -pthread &
 wait
 
 g++ -std=c++17 Test_Math_01.cc -o test_math_01 -lygor -pthread &
-g++ -std=c++17 Test_Math_02.cc -o test_math_02 -lygor -pthread &
-g++ -std=c++17 Test_Math_03.cc -o test_math_03 -lygor -pthread &
 g++ -std=c++17 Test_Math_04.cc -o test_math_04 -lygor -pthread &
 g++ -std=c++17 Test_Math_05.cc -o test_math_05 -lygor -pthread &
 wait
 
-g++ -std=c++17 Test_Math_06.cc -o test_math_06 -lygor -pthread &
 g++ -std=c++17 Test_Math_07.cc -o test_math_07 -lygor -pthread &
 g++ -std=c++17 Test_Math_08.cc -o test_math_08 -lygor -pthread &
 g++ -std=c++17 Test_Math_09.cc -o test_math_09 -lygor -pthread &
@@ -61,21 +52,14 @@ wait
 g++ -std=c++17 Test_Math_11.cc -o test_math_11 -lygor -pthread -lboost_serialization -lboost_iostreams &
 g++ -std=c++17 Test_Math_12.cc -o test_math_12 -lygor -pthread &
 g++ -std=c++17 Test_Math_13.cc -o test_math_13 -lygor -pthread &
-g++ -std=c++17 Test_Math_14.cc -o test_math_14 -lygor -pthread &
 g++ -std=c++17 Test_Math_15.cc -o test_math_15 -lygor -pthread &
 wait
 
 g++ -std=c++17 Test_Math_16.cc -o test_math_16 -lygor -pthread &
-g++ -std=c++17 Test_Math_17.cc -o test_math_17 -lygor -pthread &
-g++ -std=c++17 Test_Math_18.cc -o test_math_18 -lygor -pthread &
 g++ -std=c++17 Test_Math_19.cc -o test_math_19 -lygor -pthread &
-g++ -std=c++17 Test_Math_20.cc -o test_math_20 -lygor -pthread &
 wait
 
 g++ -std=c++17 Test_Math_21.cc -o test_math_21 -lygor -pthread &
-g++ -std=c++17 Test_Math_22.cc -o test_math_22 -lygor -pthread &
-g++ -std=c++17 Test_Math_23.cc -o test_math_23 -lygor -pthread &
-g++ -std=c++17 Test_Math_24.cc -o test_math_24 -lygor -pthread &
 wait
 
 g++ -std=c++17 Test_Math_25.cc -o test_math_25 -lygor -pthread &
@@ -92,25 +76,13 @@ wait
 
 g++ -std=c++17 Test_Misc_01.cc -o test_misc_01 -lygor -pthread  &
 g++ -std=c++17 Test_Noise_01.cc -o test_noise_01 -lygor -pthread &
-g++ -std=c++17 Test_Noise_02.cc -o test_noise_02 -lygor -pthread &
-g++ -std=c++17 Test_Serialization_01.cc -o test_serialization_01 -lygor -pthread &
 wait
 
-g++ -std=c++17 Test_Stats_01.cc -o test_stats_01 -lygor -lgsl -lgslcblas -pthread&
-g++ -std=c++17 Test_Stats_02.cc -o test_stats_02  -lygor -pthread &
-g++ -std=c++17 Test_Stats_03.cc -o test_stats_03  -lygor -pthread &
-g++ -std=c++17 Test_Stats_04.cc -o test_stats_04  -lygor -pthread &
-g++ -std=c++17 Test_Stats_05.cc -o test_stats_05  -lygor -pthread &
-wait
-
-g++ -std=c++17 Test_String_01.cc -o test_string_01 -lygor -pthread &
 g++ -std=c++17 Test_TAR_01.cc -o test_tar_01 -lygor -pthread &
 g++ -std=c++17 Test_YgorCONFIGTools_01.cc -o test_configtools_01 -lygor -pthread &
 g++ -std=c++17 Test_YgorDICOMTools_01.cc -o test_dicomtools_01 -lygor -pthread &
 wait
 
-g++ -std=c++17 Test_MeshesRemeshing.cc -o test_meshesremeshing -lygor -pthread &
-g++ -std=c++17 Test_MeshesRefinement.cc -o test_meshesrefinement -lygor -pthread &
 g++ -std=c++17 Test_MeshesBoolean.cc -o test_meshesboolean -lygor -pthread &
 wait
 
