76f2223427d6f1a958c8485c38d258382d32fa04
Hal Clark <hdclark@ualberta.ca>
2026-07-23 11:57:57 -0700

    P0: add product contract umbrella

---
+++ src/YgorMeshesBooleanProductContract.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef YGOR_MESHES_BOOLEAN_PRODUCT_CONTRACT_H_
+#define YGOR_MESHES_BOOLEAN_PRODUCT_CONTRACT_H_
+
+#include "YgorMeshesBooleanProductContractPolicies.h"
+#include "YgorMeshesBooleanProductContractResult.h"
+
+#endif
