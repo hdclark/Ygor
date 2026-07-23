16c49b0f1440b002f12088ef08ae10bd1ea68a22
Hal Clark <hdclark@ualberta.ca>
2026-07-22 19:41:17 -0700

    Add Component 07 relation foundation sources (plan 07)

---
+++ src/YgorMeshesBooleanBounded/RelationPreflight.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CanonicalCandidateStream.h"
+#include "RelationRequestGraph.h"
+
+namespace ygor::mesh_boolean::bounded {
+
+struct relation_preflight_plan final {
+  std::uint64_t candidate_count = 0;
+  std::uint64_t request_upper_bound = 0;
+  std::uint64_t dependency_upper_bound = 0;
+  std::uint64_t witness_upper_bound = 0;
+  std::uint64_t fixed_work_units = 0;
+  std::uint64_t fixed_persistent_bytes = 0;
+  std::uint64_t fixed_temporary_bytes = 0;
+};
+
+template <class T, class I>
+bool preflight_relation_foundation(
+    const canonical_candidate_stream<T, I> &candidates,
+    const relation_capabilities &capabilities, relation_preflight_plan &plan,
+    bounded_boolean_error &error);
+
+} // namespace ygor::mesh_boolean::bounded
