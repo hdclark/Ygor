b19dacc529b1f391de94ea8e8779cb8e2911f7de
Hal Clark <hdclark@ualberta.ca>
2026-07-22 23:36:55 -0700

    Register Component 07 source-edge qualification (plan 07)

---
+++ tests/mesh_boolean_bounded/Component07SourceEdge.cmake
@@ -0,0 +1,10 @@
+add_executable(ygor_mesh_boolean_source_edge_relation_tests
+    "${CMAKE_SOURCE_DIR}/tests/mesh_boolean_bounded/TestSourceEdgeRelations.cc")
+target_include_directories(ygor_mesh_boolean_source_edge_relation_tests PRIVATE
+    "${CMAKE_SOURCE_DIR}/src"
+    "${CMAKE_SOURCE_DIR}/tests/mesh_boolean_bounded")
+target_link_libraries(ygor_mesh_boolean_source_edge_relation_tests PRIVATE
+    ygor_mesh_boolean_bounded_strict ygor Threads::Threads)
+ygor_apply_mesh_boolean_strict_fp(ygor_mesh_boolean_source_edge_relation_tests)
+add_test(NAME mesh_boolean_bounded_component07_source_edge_relations
+    COMMAND ygor_mesh_boolean_source_edge_relation_tests)
